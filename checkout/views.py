14e22f7f54fe05fc983035d6cb8f53981affc4b3
DjPaulP <Paul@paulprenderville.ie>
2021-10-12 12:47:51 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jjk1tLoLAoW8DAN10Tq5Xp5CBLIZY5qtME8l17V730Xny2itpGUaMyEsKUWXkGAyneRPTe2htn7Nu7H4WoSURQ000PV6hiaFn',
+        'client_secret': 'Test Client Secret',
     }
 
     return render(request, template, context)
